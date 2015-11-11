bf5542ec026eab0111ff0e6a3526e02ddfadb468
geguabi <gergaly.gabor@gmail.com>
2015-11-11 14:04:00 +0000

    1.9.4

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var recipeRouter = require('./controllers/recipes');
 var commentRouter = require('./controllers/comments');
 var loginRouter = require('./controllers/login')
 
-var MongoStore = require('connect-mongo')(express);
+
 var RedisStore = require('connect-redis')(express);
 
 
